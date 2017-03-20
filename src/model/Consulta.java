644327a30bcf6a1274ce602bc2af53530a9c3264
Luís Narciso <Lus Narciso@LNarciso>
2017-03-20 16:49:16 +0000

    asdf

---
+++ src/model/Consulta.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Consulta {
+	
+	private String data;
+	private String descricao;
+	public Consulta(String data, String descricao) {
+		super();
+		this.data = data;
+		this.descricao = descricao;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
